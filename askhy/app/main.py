6d1049a50a0f55cdb4c088b13067a6aa3ae6aa54
Prev <prevdev@gmail.com>
2017-12-02 00:33:38 +0900

    Implement intercommunication between python and mysql

--- askhy/app/main.py
+++ askhy/app/main.py
@@ -1,9 +1,30 @@
 from flask import Flask
+import os
+
+from core.dbdriver import get_db, init_tables
+
 app = Flask(__name__)
+
+init_tables()
 
 @app.route('/')
 def index():
-    return 'Hello World, Hello Docker!'
+	html = "<h1>부탁하냥</h1><ul>"
+
+	with get_db().cursor() as cursor :
+		sql = "SELECT *, (SELECT COUNT(*) FROM `cheer` WHERE ask_id = ask.id) AS cheer_cnt FROM `ask`"
+
+		cursor.execute(sql)
+		result = cursor.fetchall()
+
+		for id, message, created_time, cheer_cnt in result :
+			html += "<li>%d: %s (%d)</li>" % (id, message, cheer_cnt)
+
+	return html
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=80)
+	app.run(
+		host='0.0.0.0',
+		debug=True,
+		port=os.environ.get('APP_PORT', 8080)
+	)
