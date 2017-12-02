c1ee43d8f1d05882c6a03db4a00232e755d08602
Prev <prevdev@gmail.com>
2017-12-02 17:38:44 +0900

    Implement `POST` feature of `ask` and `cheer`

--- askhy/app/main.py
+++ askhy/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 
 from core.dbdriver import get_db, init_tables
@@ -10,15 +10,37 @@ init_tables()
 @app.route('/')
 def index():
 	with get_db().cursor() as cursor :
-		sql = "SELECT *, (SELECT COUNT(*) FROM `cheer` WHERE ask_id = ask.id) AS cheer_cnt FROM `ask`"
-
-		cursor.execute(sql)
+		cursor.execute("SELECT *, (SELECT COUNT(*) FROM `cheer`WHERE ask_id = ask.id) AS cheer_cnt FROM `ask`")
 		result = cursor.fetchall()
-	print(result)
 
 	return render_template('main.html',
 		dataset=result,
 	)
+
+@app.route('/ask', methods=['POST'])
+def add_ask():
+	message = request.form.get('message')
+
+	with get_db().cursor() as cursor :
+		sql = "INSERT INTO `ask` (`message`) VALUES (%s)"
+		r = cursor.execute(sql, (message, ))
+
+	return render_template('redirection.html', success=r)
+
+
+@app.route('/cheer', methods=['POST'])
+def add_cheer():
+	ask_id = request.form.get('ask_id')
+	message = request.form.get('message')
+
+	with get_db().cursor() as cursor :
+		sql = "INSERT INTO `cheer` (`ask_id`, `message`) VALUES (%s, %s)"
+		r = cursor.execute(sql, (ask_id, message))
+
+	return render_template('redirection.html', success=r)
+
+
+
 
 if __name__ == '__main__':
 	app.run(
