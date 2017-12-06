04177ce8639f8bba1fd78e0a28b3596f9f6bd845
Prev <prevdev@gmail.com>
2017-12-06 21:08:05 +0900

    Improve application

--- askhy/app/main.py
+++ askhy/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import os
 
 from core.dbdriver import get_db, init_tables
@@ -17,27 +17,55 @@ def index():
 		dataset=result,
 	)
 
+
+@app.route('/ask/<int:ask_id>', methods=['GET'])
+def view_ask(ask_id):
+	conn = get_db()
+
+	with conn.cursor() as cursor :
+		cursor.execute("SELECT * FROM `ask` WHERE id = %s", (ask_id, ))
+		row = cursor.fetchone()
+
+		cursor.execute("SELECT * FROM `cheer` WHERE ask_id = %s", (ask_id, ))
+		rows2 = cursor.fetchall()
+
+		print(rows2)
+
+	return render_template('detail.html',
+		message=row[1],
+		register_time=row[2],
+		cheers=rows2,
+	)
+
+
 @app.route('/ask', methods=['POST'])
 def add_ask():
+	conn = get_db()
 	message = request.form.get('message')
 
-	with get_db().cursor() as cursor :
+	with conn.cursor() as cursor :
 		sql = "INSERT INTO `ask` (`message`) VALUES (%s)"
 		r = cursor.execute(sql, (message, ))
 
-	return render_template('redirection.html', success=r)
+	id = conn.insert_id()
+	conn.commit()
+
+	return redirect("/#a" + str(id))
 
 
 @app.route('/cheer', methods=['POST'])
 def add_cheer():
+	conn = get_db()
 	ask_id = request.form.get('ask_id')
 	message = request.form.get('message')
 
-	with get_db().cursor() as cursor :
+	with conn.cursor() as cursor :
 		sql = "INSERT INTO `cheer` (`ask_id`, `message`) VALUES (%s, %s)"
 		r = cursor.execute(sql, (ask_id, message))
 
-	return render_template('redirection.html', success=r)
+	conn.commit()
+
+	return redirect("/#c" + ask_id)
 
 
 
