15d6fe9c8fc766b249eeea8e80bfcf5281a669ad
Prev <prevdev@gmail.com>
2017-12-07 14:57:53 +0900

    Get arcus info from ENV, instead of hard-embedded

--- askhy/app/arcus_test.py
+++ askhy/app/arcus_test.py
@@ -1,18 +1,23 @@
 from arcus import *
 from arcus_mc_node import ArcusMCNodeAllocator, EflagFilter
 
-connection_url = '172.17.0.5:2181'
-service_code = 'ruo91-cloud'
+import os
+
+connection_url = os.environ.get('ARCUS_URL')
+service_code = os.environ.get('ARCUS_SERVICE_CODE')
 timeout = 20
 
 client = Arcus(ArcusLocator(ArcusMCNodeAllocator(ArcusTranscoder())))
 
 client.connect(connection_url, service_code)
 
-ret = client.set('test:ohoohohohoh', 'test...', timeout)
+testmsg = "My Test Message"
+
+ret = client.set('test:ohoohohohoh', testmsg, timeout)
 print(ret.get_result())
 
 ret = client.get('test:ohoohohohoh')
 print(ret.get_result())
 
 
+assert testmsg == ret.get_result()
--- askhy/run_app.sh
+++ askhy/run_app.sh
@@ -4,9 +4,12 @@ docker rm -f askhy
 
 docker run -p 8080:80 \
   --link mysql:mysql_host \
+  --link arcus-admin:arcus_host \
   -e DATABASE_HOST=mysql_host \
   -e DATABASE_USER=root \
   -e DATABASE_PASS=root \
   -e DATABASE_NAME=test \
+  -e ARCUS_URL=arcus_host:2181 \
+  -e ARCUS_SERVICE_CODE=ruo91-cloud \
   --name askhy \
   askhy
